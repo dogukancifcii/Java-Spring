c4bc9f159cdd29ccd8e9248c943ba9b140822533
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-08-13 22:58:56 +0300

    config2

--- SpringMVC/src/main/java/com/dogukan/RootConfig.java
+++ SpringMVC/src/main/java/com/dogukan/RootConfig.java
@@ -1,29 +1,49 @@
 package com.dogukan;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @PropertySource("classpath:db.properties")
 //data base ayarlamalarını kaynak uzerinden yaptık ve o kaynagı okumak icin bir kaynak okuyucuya ihtiyacimiz var
 @Configuration
 public class RootConfig {
+
+    @Autowired
+    private Environment environment;
+
     //JDBC ve Hibernate
     @Bean
     public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource();
-        sessionFactory.setHibernateProperties();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setHibernateProperties(hibernateProperties());
         sessionFactory.setPackagesToScan(new String[]{"com.dogukan.domain"});//entity'lerin arandigi yer
         return sessionFactory;
     }
 
     private DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        
+        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        return properties;
     }
 }
