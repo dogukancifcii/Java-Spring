8dd089f265c167f775afd829704eac6318a72a02
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-08-17 20:40:59 +0300

    Properties

--- SpringBootIntro/src/main/java/com/dogukan/SpringBootIntroApplication.java
+++ SpringBootIntro/src/main/java/com/dogukan/SpringBootIntroApplication.java
@@ -1,13 +1,18 @@
 package com.dogukan;
 
+import com.dogukan.domain.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootIntroApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootIntroApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIntroApplication.class, args);
+
+        Student student =new Student();
+        student.getName();
+        student.setLastname("cifci");
+    }
 
 }
---
+++ SpringBootIntro/src/main/java/com/dogukan/domain/Student.java
@@ -0,0 +1,55 @@
+package com.dogukan.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Getter //tum fieldlar icin getter method
+@Setter //tum fieldlar icin setter method
+@AllArgsConstructor //tum parametrelerin oldugu const olusturma
+@NoArgsConstructor //parametresiz const olusturma
+//@RequiredArgsConstructor:final ile isaretlenen fieldlari parametre olarak alan constructor
+/*
+    public Student(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+ */
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE) //Id setter methodunu iptal etmis olduk
+    private Long id;
+
+    @NotBlank(message = "Name can not be blank") //request aninda dogrulama yapar
+    @Size(min = 2, max = 50, message = "name must be between 2 and 50")//Size karakter icin kullanilir.
+    @Column(nullable = false)
+    /*final*/ private String name;
+
+    @NotBlank(message = "Name can not be blank") //request aninda dogrulama yapar
+    @Size(min = 2, max = 50, message = "name must be between 2 and 50")//Size karakter icin kullanilir.
+    @Column(nullable = false)
+    /*final*/ private String lastname;
+
+    @NotNull(message = "grade can not be null")//notBlank ve NotEnty sayisal degerde kullanilamaz
+    @Column(nullable = false)
+    private Integer grade;
+
+    @Column(unique = true)
+    @Email(message = "please provide valid email!")//aa@bbb.ccc email formatinda olmasini dogrular
+    //@Pattern()
+    private String email;
+
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime createDate = LocalDateTime.now();
+
+    //getter-setter: boilerplate code
+}
