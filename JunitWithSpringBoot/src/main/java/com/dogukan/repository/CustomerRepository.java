9e83107dfa59bf8a8887044090c1b7440f9bef9a
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-08-28 21:58:16 +0300

    Base class

---
+++ JunitWithSpringBoot/src/main/java/com/dogukan/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.dogukan.repository;
+
+import com.dogukan.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+}
