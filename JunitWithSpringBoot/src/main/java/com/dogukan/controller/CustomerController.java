0e101ae255db51aaafdfc40dc75e42a6cd83b3cf
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-08-28 22:09:04 +0300

    getCustomerById

--- JunitWithSpringBoot/src/main/java/com/dogukan/controller/CustomerController.java
+++ JunitWithSpringBoot/src/main/java/com/dogukan/controller/CustomerController.java
@@ -1,7 +1,27 @@
 package com.dogukan.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.dogukan.domain.Customer;
+import com.dogukan.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/customers")
+@RequiredArgsConstructor
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    //read-delete
+
+    //http://localhost:8080/customers/1 + GET
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable() Long id) {
+
+        Customer customer = customerService.getCustomerById(id);
+
+        return ResponseEntity.ok(customer);
+    }
+
 }
--- JunitWithSpringBoot/src/main/java/com/dogukan/service/CustomerService.java
+++ JunitWithSpringBoot/src/main/java/com/dogukan/service/CustomerService.java
@@ -1,7 +1,21 @@
 package com.dogukan.service;
 
+import com.dogukan.domain.Customer;
+import com.dogukan.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public Customer getCustomerById(Long id) {
+
+        return customerRepository.findById(id).orElseThrow(() -> new RuntimeException("Lutfen gecerli bir Id giriniz" + id));
+
+    }
+
+    
 }
